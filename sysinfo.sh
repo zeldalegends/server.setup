57418a7652f1493b10e9fd91368a516c479c93bc
alinonet <101158367+alinonet@users.noreply.github.com>
2023-03-16 13:56:05 +0100

    Update sysinfo.sh

--- sysinfo.sh
+++ sysinfo.sh
@@ -1,31 +1,46 @@
 #!/bin/bash
 
-echo -e "Kernel"
-cat /etc/issue 
-# echo -e "System"
-# uname -nrpv
-# echo
-echo -e "CPU"
+echo -e "\033[1;32mSYSTEM INFORMATION \033[0m"
+KERNEL=$(sudo cat /etc/issue)
+echo ${KERNEL::-6}
+echo IP: $(curl -s https://ifconfig.me)
+uname -nrpv
+echo
+echo -e "\033[1;33mCPU \033[0m"
 cat /proc/cpuinfo|grep name
 echo
-echo -e "RAM"
+echo -e "\033[1;34mRAM\033[0m"
 free -m
 echo
-echo -e "Disks"
+echo -e "\033[1;35mDISKS \033[0m"
 df -h
 echo
-echo -e "Processes"
-ps aux # acrux
+read key
+clear
+
+echo -e "\033[1;36mDOCKER CONTAINERS \033[0m"
+docker ps -a --format "table {{.Names}}\t{{.ID}}\t{{.Status}}\t{{.Ports}}\t{{.Image}}" | sort # LC_COLLATE=C sort
 echo
-echo -e "Ports"
-sudo ss -tulpn
+echo -e "\033[1;30mPORTS \033[0m"
+lsof -i -P -n | grep LISTEN # ss -tunlp
 echo
-echo -e "User and groups"
-id
+echo -e "\033[1;33mCOMMANDS \033[0m"
+echo -e "docker rm -f \$(docker ps -aq) \033[1;32m# Remove all Docker containers \033[0m"
 echo
-echo -e "Docker"
-docker ps -a --format "table {{.ID}}\t{{.Names}}\t{{.Status}}\t{{.Ports}}"
+echo -e "CTRL+Z \033[1;32m# Put the running process in background \033[0m"
+echo -e "jobs \033[1;32m# List the processes running in background \033[0m"
+echo -e "fg \"%<n>\" \033[1;32m# Recall the <n>th process in background to foreground \033[0m"
 echo
-echo Press Enter to see the real-time monitor...
+echo -e "sudo nc -l <port> \033[1;32m# Open a local socket on a specific port \033[0m"
+echo -e "telnet <host> <post> \033[1;32m# Connect to a remote socket on a specific port \033[0m"
+echo
+echo -e "\033[1;34mUSERS \033[0m"
+sudo cat /etc/passwd | grep 100 | grep -v 100:
+echo
+echo -e "\033[1;35mPROCESSES \033[0m"
+ps acrux
+
+alias tree="ls -aR | grep ':$' | perl -pe 's/:$//;s/[^-][^\/]*\//    /g;s/^    (\S)/└── \1/;s/(^    |    (?= ))/│   /g;s/    (\S)/└── \1/'"
+
 read key
 htop
