79499d10565b41fdc70678e21c6d85a2923cb112
alinonet <101158367+alinonet@users.noreply.github.com>
2023-03-16 14:16:29 +0100

    Update install.sh

--- docker/install.sh
+++ docker/install.sh
@@ -2,41 +2,46 @@
 
 # Source: https://docs.docker.com/engine/install/ubuntu/#installation-methods
 
-# Setup Docker repository
-sudo apt-get update
-sudo apt-get install \
+# Uninstall old versions
+sudo apt remove docker docker-engine docker.io containerd runc
+
+# Setup the repository
+sudo apt update
+sudo apt install \
     ca-certificates \
     curl \
     gnupg \
     lsb-release
 
-sudo mkdir -p /etc/apt/keyrings
+sudo mkdir -m 0755 -p /etc/apt/keyrings
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
 
-echo \
-  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
   $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 
 # Install Docker Engine
-sudo apt-get update
+sudo apt update
+sudo apt install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 
-sudo apt-get install docker-ce docker-ce-cli containerd.io docker-compose-plugin
+# Manage Docker as a non-root user
+# groupadd docker # (already created in Ubuntu)
+sudo adduser $USER docker # sudo usermod -aG docker $USER
+newgrp docker # activate the changes to groups
+sudo service docker restart # sudo systemctl restart docker.service
 
-# Running Docker as normal user
-sudo groupadd docker
-sudo usermod -aG docker $USER
-newgrp docker
-sudo service docker restart
+# Configure Docker to start on boot with systemd # (already set in Ubuntu) 
+# sudo systemctl enable docker.service
+# sudo systemctl enable containerd.service
 
-# Start on boot
-sudo systemctl enable docker.service
-sudo systemctl enable containerd.service
-
-# Disable start on boot
+# Disable start Docker on boot
 # sudo systemctl disable docker.service
 # sudo systemctl disable containerd.service
 
 # Verify installations
 docker info
+# systemctl status docker.service
+sudo service docker status
 
-echo If Docker daemon is not started, run \"sudo reboot\". 
+echo Press Enter to reboot, CTRL+C to terminate...
+read key
+sudo reboot 
